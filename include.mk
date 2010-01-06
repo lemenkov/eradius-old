5298d44a613ec9e28adca973dc101ac0283e76a6
Maxim Treskin <zerthurd@gmail.com>
2010-01-06 16:37:28 +0600

    Standalone building (not depends on jungerl)

---
+++ include.mk
@@ -0,0 +1,7 @@
+EBIN_DIR := ../ebin
+DOC_DIR  := ../doc
+EMULATOR := beam
+
+ERL_SOURCES := $(wildcard *.erl)
+ERL_HEADERS := $(wildcard *.hrl) $(wildcard ../include/*.hrl)
+ERL_OBJECTS := $(ERL_SOURCES:%.erl=$(EBIN_DIR)/%.$(EMULATOR))
